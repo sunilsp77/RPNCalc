053ca0abd574965942edf2e816f1cb3cf95850cf
sunilsp77 <sunil.thind17@gmail.com>
2017-05-13 10:25:37 +0530

    FirstCheckIn

---
+++ RPNCalc/RPNCalculator.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace RPNCalc
+{
+    class RPNCalculator
+    {
+        Stack<int> st = new Stack<int>();
+        public string finalres;
+        public string Calculate(string arg)
+        {
+            string[] arr = arg.Split(',');
+            try
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i].Equals("+") || arr[i].Equals("-") || arr[i].Equals("*") || arr[i].Equals("/"))
+                    {
+                        if (st.Count < 2)
+                        {
+                            throw new Exception("Invalid Expression");
+                        }
+                        int res;
+                        int a = st.Pop();
+                        int b = st.Pop();
+                        if (a >= Int32.MaxValue || b >= Int32.MaxValue)
+                            throw new Exception("greater than max");
+                        if (arr[i].Equals("+"))
+                        {
+                            res = b + a;
+                            if (res >= int.MaxValue)
+                            {
+                                finalres = "greater than max";
+                                throw new Exception(finalres);
+                            }
+                        }
+                        else if (arr[i].Equals("-"))
+                        {
+                            res = b - a;
+                        }
+                        else if (arr[i].Equals("*"))
+                        {
+                            res = b * a;
+                        }
+                        else if (arr[i].Equals("/"))
+                        {
+                            if (a == 0)
+                            {
+                                finalres = "Divide by zero";
+                                throw new Exception(finalres);
+                            }
+                            res = b / a;
+                        }
+                        else
+                        {
+                            finalres = "Invalid operator";
+                            throw new Exception(finalres);
+                        }
+                        st.Push(res);
+                    }
+                    else if (arr[i].Equals("!"))
+                    {
+                        if (st.Count < 1)
+                        {
+                            finalres = "Invalid Expression";
+                            throw new Exception(finalres);
+                        }
+                        int res;
+                        int a = st.Pop();
+                        if (arr[i].Equals("!"))
+                        {
+                            if (a < 0)
+                            {
+                                finalres = "Invalid Operand";
+                                throw new Exception(finalres);
+                            }
+
+                            int fact_result = calculate_fact(a);
+                            res = fact_result;
+                        }
+                        else
+                        {
+                            finalres = "Invalid operator";
+                            throw new Exception(finalres);
+                        }
+                        st.Push(res);
+                    }
+                    else
+                    {
+                        st.Push(Convert.ToInt32(arr[i]));
+                    }
+
+                }
+                finalres = st.Pop().ToString();
+            }
+            catch (Exception e)
+            {
+               
+            }
+            return finalres;
+        }
+        public int calculate_fact(int a)
+        {
+            if(a==0)
+            {
+                return 1;
+            }
+            return a * calculate_fact(a - 1);
+        }
+            
+       }
+    
+}
---
+++ RPNCalc/TestRPN.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RPNCalc
+{
+    [TestFixture]
+    class TestRPN
+    {
+        [Test]
+        public void TestCase1()
+        {
+            RPNCalculator obj = new RPNCalculator();
+            Assert.AreEqual(4, Convert.ToInt32(obj.Calculate("2,2,+")));
+        }
+        [Test]
+        public void TestCase2()
+        {
+            RPNCalculator obj = new RPNCalculator();
+            Assert.AreEqual(5, Convert.ToInt32(obj.Calculate("2,3,+")));
+        }
+        [Test]
+        public void TestCase3()
+        {
+            try
+            {
+                RPNCalculator obj = new RPNCalculator();
+                int result = Convert.ToInt32(obj.Calculate("2,+,2"));
+            }catch(Exception ex)
+            {
+                Assert.AreEqual("Invalid Expression", ex.Message);
+            }
+            
+        }
+        [Test]
+        public void TestCase4()
+        {
+            try
+            {
+                RPNCalculator obj = new RPNCalculator();
+                int result = Convert.ToInt32(obj.Calculate("+"));
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Invalid Expression", ex.Message);
+            }
+            
+        }
+        [Test]
+        public void TestCase5()
+        {
+                RPNCalculator obj = new RPNCalculator();           
+                Assert.AreEqual(0,Convert.ToInt32(obj.Calculate("2,2,+,4,-")));
+        }
+        [Test]
+        public void TestCase6()
+        {
+            RPNCalculator obj = new RPNCalculator();
+            Assert.AreEqual(1, Convert.ToInt32(obj.Calculate("2,2,*,4,/")));
+        }
+        [Test]
+        public void TestCase7()
+        {
+            RPNCalculator obj = new RPNCalculator();
+            Assert.AreEqual(0, Convert.ToInt32(obj.Calculate("2,2,*,4,/,1,+,2,-")));
+        }
+        [Test]
+        public void TestCase8()
+        {
+            try
+            {
+                RPNCalculator obj = new RPNCalculator();
+                int result = Convert.ToInt32(obj.Calculate("2,0,/"));
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Divide by zero", ex.Message);
+            }
+        }
+        [Test]
+        public void TestCase9()
+        {
+            try
+            {
+                RPNCalculator obj = new RPNCalculator();
+                Convert.ToInt32(obj.Calculate("-4,!"));
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Invalid Operand", ex.Message);
+            }
+        }
+        [Test]
+        public void TestCase10()
+        {
+            RPNCalculator obj = new RPNCalculator();
+            Assert.AreEqual(24, Convert.ToInt32(obj.Calculate("4,!")));
+        }
+        [Test]
+        public void TestCase11()
+        {
+            RPNCalculator obj = new RPNCalculator();
+            Assert.AreEqual(1, Convert.ToInt32(obj.Calculate("2,2,*,4,/,1,+,2,-,!")));
+        }
+        [Test]
+        public void TestCase12()
+        {
+            try
+            {
+                RPNCalculator obj = new RPNCalculator();
+                obj.Calculate("2147483647,2,+");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("greater than max1", ex.Message);
+            }
+        }
+    }
+}
